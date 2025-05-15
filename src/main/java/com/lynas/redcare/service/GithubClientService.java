7765117f2cb54386abbd8db049c7ca25445837cf
Sazzad Islam <szlynas@gmail.com>
2025-05-15 13:18:45 +0200

    Optimize code

--- src/main/java/com/lynas/redcare/service/GithubClientService.java
+++ src/main/java/com/lynas/redcare/service/GithubClientService.java
@@ -19,14 +19,17 @@ public class GithubClientService {
     private static final Logger LOGGER = LoggerFactory.getLogger(GithubClientService.class);
 
     public GithubRepositoryApiResponseDto getRepositoryInfo(String language, LocalDate lastUpdatedAt) {
-        /*var uri = UriComponentsBuilder.newInstance()
+        /*
+        var uri = UriComponentsBuilder.newInstance()
                 .path("/search/repositories?q=language:")
                 .path(language)
                 .path(" created:>")
                 .path(lastUpdatedAt.toString())
-                .toUriString();*/
-        // TODO URL encoded value does not work and return bad request from GithubAPI
-        // TODO e.g. /search/repositories%3Fq=language:Java%20created:%3E2025-04-01 returns 404 NOT_FOUND
+                .toUriString();
+
+        URL encoded value does not work when calling GitHub API
+        e.g. /search/repositories%3Fq=language:Java%20created:%3E2025-04-01 returns 404 NOT_FOUND
+        */
         var uri = "/search/repositories?q=language:" + language + " created:>" + lastUpdatedAt;
         LOGGER.info(uri);
         return restClient.get()
